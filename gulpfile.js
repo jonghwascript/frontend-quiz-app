6d461e782d94702ea93716dde8caf743cfd8234f
jonghwa <jonghwa@mibank.me>
2026-09-22 20:10:03 +0900

    refactor(quiz): extract shared header into Gulp page templates
    Move page sources into src/pages and parameterize the shared header. Add HTML include processing and template watching. Update page markup with a progress element, answer form, and presentation classes.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css'); // CSS 압축용
+const fileInclude = require('gulp-file-include');
 
 // 포맷 대상 파일
 const PRETTIER_GLOBS = ['src/scss/**/*.scss', '*.html', 'gulpfile.js'];
@@ -15,6 +16,20 @@ function scssTask() {
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('css')); // CSS 출력 위치
 }
+
+function htmlTask() {
+  return gulp
+    .src('src/pages/*.html') // 페이지 원본 읽기
+    .pipe(
+      fileInclude({
+        prefix: '@@',
+        basepath: '@file', // include 경로는 호출한 파일 기준
+      }),
+    )
+    .pipe(gulp.dest('.')); // 완성된 HTML을 프로젝트 루트에 저장
+}
+
+exports.html = htmlTask;
 
 // Prettier로 코드 포맷팅 (gulp-prettier는 ESM 전용이라 동적 import 필요)
 async function prettierTask() {
@@ -32,9 +47,17 @@ async function prettierTask() {
 // SCSS 변경 감시
 function watchTask() {
   gulp.watch('src/scss/**/*.scss', scssTask);
+
+  gulp.watch(['src/pages/**/*.html', 'src/partials/**/*.html'], htmlTask);
 }
 
 exports.prettier = prettierTask;
 
 // 기본 태스크
-exports.default = gulp.series(prettierTask, scssTask, watchTask);
+// exports.default = gulp.series(prettierTask, scssTask, watchTask);
+
+exports.default = gulp.series(
+  prettierTask,
+  gulp.parallel(scssTask, htmlTask),
+  watchTask,
+);
